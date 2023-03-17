6be19b34a56f5f6a9354b2de05c06917b145403a
Naveendubey93 <naveen.parasi@gmail.com>
2023-03-17 14:54:58 +0000

    resolving -conflicts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
 import './App.css';
+// import About from './components/About';
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import About from './components/About';
 function App() {
+  const [mode, setMode] = useState('dark');
+  const [textColor, setColor] = useState('black');
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type,
+    })
+    setTimeout(() => {
+      setAlert(null);
+    },2000)
+  }
+  const toggleMode = () => {
+    if (mode === 'dark') {
+      setMode('light');
+      setColor('black')
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light Mode has been enabled', 'success');
+      document.title = 'TextUtils- light mode'
+    } else {
+      setMode('dark');
+      setColor('white');
+      document.body.style.backgroundColor = '#042743';
+      showAlert('Dark Mode has been enabled','success');
+        document.title = 'TextUtils- dark mode'
+    } 
+  }
+  console.log("setColor = == = = = >", textColor);
   return (
-    <div className="class">
-      <Navbar title = "Text-Project"/>
-      <div className="container">
-        <TextForm title = "Enter text"/>
-      </div>
-    </div>
+  
+    <div> 
+        <Router>
+      <Navbar title = "Text-Project" mode={mode} toggleMode={toggleMode}  textColor={textColor}/>
+      <Alert alert = {alert} />
+      <div className="my-3">
+      <Routes>
+        <Route exact path="/about" element = 
+          {<About />} /> 
+          <Route exact path="/" element = { 
+          <TextForm  showAlert = {showAlert} title = "Enter the text to analyze below" textColor={textColor}/>
+        } />
+         </Routes>
+        </div>
+  
+     </Router>
+     </div> 
   );
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,40 +1,72 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 export default function TextForm(props) {
-
+console.log("received props = == = = =>", props);
   const handleOnChange = (event) => {
     setText(event.target.value);
   }
   const handleOnClick = () => {
     setText(text.toUpperCase());
+    props.showAlert('Converted to Upper case','success')
   }
   const handleLoClick = () => {
     setText(text.toLocaleLowerCase());
+    props.showAlert('Converted to lower case','success')
   }
   const handleUcClick = () => {
     setText(text[0].toUpperCase() + text.slice(1));
+    props.showAlert('Converted to First lettr case','success')
   }
-  
-
+  const removeExtraSpace = () => {
+    setText(text.split(' ').filter(word => word).join(' '));
+    props.showAlert('Removed extra spaces','success')
+  } 
+  const copyText = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert('Copied to clipboard','success')
+  }
+  const resetText = () => {
+    if(text.length) {
+      setText('');
+      props.showAlert('Test reset','success')
+    }
+  }
   const [text, setText] = useState('Enter text Here');
   return (
-    <div>
+    <div className="container">
       <div className="mb-3">
-        <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.title} </label>
+        {/* <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.title} </label> */}
+        <h2  style={{color: props.textColor || 'black'}}> {props.title || "Enter the text to analyze below1"} </h2>
         <textarea className="form-control" id="myBox" rows="3"  onChange = {handleOnChange} value = {text}></textarea>
       </div>
-      <button className="btn btn-primary mx-1" onClick={handleOnClick}>Convert to upper case</button>
-      <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to lower case</button>
-      <button className="btn btn-primary mx-1" onClick={handleUcClick}>UC First case</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleOnClick}>Convert to upper case</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to lower case</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleUcClick}>UC First case</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={removeExtraSpace}>Remove extra space</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={copyText}>Copy Text</button>
+      <button className="btn btn-primary mx-1 my-1" onClick={resetText}>Reset Text</button>
 
+        <h2  style={{color: props.textColor}}>Your text summery</h2>
 
-        <div className="container">
-          <h2>Your text summery</h2>
-          <p> Total {text.length} letters and {text.split(" ").length } words</p>
+        <div className="container my-3" style={{color: props.textColor}}>
+          <p> Total {text.split(" ").filter(res=> res.length!==0).length } words  and {text.trim().length} characters </p>
+          <p> Total {0.008 * text.split(" ").filter(res=> res.length!==0).length } Minutes read </p>
+
         </div>
-        <h2>Prevew</h2>
-        <p>{text}</p>
+        <h2 style={{color: props.textColor}}>Preview</h2>
+        <p style={{color: props.textColor}}>{text}</p>
 
     </div>
 
   )
 }
+
+TextForm.propTypes = {
+  title: PropTypes.string,
+  textColor: PropTypes.string
+}
+TextForm.defaultProps = {
+  title: "Enter the text to analyze below",
+  textColor: 'black'
+}
+
